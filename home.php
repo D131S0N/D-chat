42e35c511492baf68b9048d6aa857ae3cf56a853
D131S0N <di.eison95@outlook.com>
2018-02-09 19:56:28 -0200

    home

---
+++ home.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="css/changes.css" rel="stylesheet"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/select2.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/select2-bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
+        <script src="js/moment.min.js" type="text/javascript"></script>
+        <script src="js/pt-br.js" type="text/javascript"></script>
+        <script src="js/transition.js" type="text/javascript"></script>
+        <script src="js/collapse.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/select2.min.js" type="text/javascript"></script>
+
+        <title>Notas online</title>
+    </head>
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default" id="cabecalho">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="home.php">
+                        <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+                        Home <span class="sr-only">(current)</span></a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="criar_sala.php">Criar sala</a></li>
+                </ul>
+            </nav>
+        </div>
+    </body>
+</html>
