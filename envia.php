e37307dc9dd568374a6c37fd98ae710c301d7f05
D131S0N <di.eison95@outlook.com>
2018-03-20 18:54:44 -0300

    criando metodo para enviar mensagem

---
+++ envia.php
@@ -0,0 +1,38 @@
+<?php
+// Incluimos o arquivo de conexão
+include 'conecta.php';
+include 'links.php';
+// Recuperamos os valores dos campos através do método POST
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$mensagem = $_POST["mensagem"];
+// Verifica se o nome foi preenchido
+/*if (empty($nome)) {
+	echo "Escreva seu nome";
+} 
+// Verifica se o email é válido
+elseif (!eregi("^[a-z0-9_\.\-]+@[a-z0-9_\.\-]*[a-z0-9_\-]+\.[a-z]{2,4}$", $email)) {
+	echo "Digite um email válido";
+} */
+// Verifica se a mensagem foi digitada
+if (empty($mensagem)) {
+	echo "Escreva uma mensagem";
+} 
+// Verifica se a mensagem nao ultrapassa o limite de caracteres
+elseif (strlen($mensagem) > 500) {
+	echo "A mensagem deve ter no máximo 500 caracteres";
+} 
+// Se não houver nenhum erro
+else {
+	// Inserimos no banco de dados
+	$query = mysqli_query("INSERT INTO messages VALUES ('', '".$nome."', '".$email."', '".$mensagem."')");
+	// Se inserido com sucesso
+	if ($query) {
+		echo false;
+	} 
+	// Se houver algum erro ao inserir
+	else {
+		echo "Não foi possível inserir a mensagem no momento.";
+	}
+}
+?>
--- sala.php
+++ sala.php
@@ -4,12 +4,64 @@ include 'links.php';
 ?>
 <html>
 <head>
-	<title>Chat</title>
+    <title>Chat</title>
+    
+<script type="text/javascript" language="javascript">
+$(function($) {
+	// Quando o formulário for enviado, essa função é chamada
+	$("#formulario").submit(function() {
+		// Colocamos os valores de cada campo em uma váriavel para facilitar a manipulação
+		var nome = $("#nome").val();
+		var email = $("#email").val();
+		var mensagem = $("#mensagem").val();
+		// Exibe mensagem de carregamento
+		$("#status").html("<img src='loader.gif' alt='Enviando' />");
+		// Fazemos a requisão ajax com o arquivo envia.php e enviamos os valores de cada campo através do método POST
+		$.post('envia.php', {nome: nome, email: email, mensagem: mensagem }, function(resposta) {
+				// Quando terminada a requisição
+				// Exibe a div status
+				$("#status").slideDown();
+				// Se a resposta é um erro
+				if (resposta != false) {
+					// Exibe o erro na div
+					$("#status").html(resposta);
+				} 
+				// Se resposta for false, ou seja, não ocorreu nenhum erro
+				else {
+					// Exibe mensagem de sucesso
+					$("#status").html("Mensagem enviada com sucesso!");
+					// Coloca a mensagem no div de mensagens
+					$("#mensagens").prepend("<strong>"+ nome +"</strong> disse: <em>" + mensagem + "</em><br />");
+					// Limpando todos os campos
+					$("#nome").val("");
+					$("#email").val("");
+					$("#mensagem").val("");
+				}
+		});
+	});
+});
+</script>
 </head>
 <body>
-    <div class="container" id="major">
-        <div class="cool-md-8" id"chatt">
-            
+    <div class="container major">
+        <div class="col-lg-10 chatt">
+            aaaaaaaaaaaaaaaaaaaaaaaaaa
+        </div>
+            <br>
+            <hr class="col-lg-11 linha">
+            <br>
+        <div class="col-lg-10 msg">
+            <form id="formulario" action="javascript:func()" method="post">
+                <input class="col-lg-11 messenger" type="text" name="mensagem">
+                <button class="btn btn-info col-lg-1 send_btn" type="submit">Send</button>
+            </form>
+            <?php
+                // Buscamos e exibimos as mensagens já contidas no banco de dados
+                $query = mysqli_query("SELECT m.* FROM messages m JOIN users AS u ON u.id = m.user_id WHERE u.id = 10");
+                while($mensagem = mysql_fetch_object($query)) {
+                    echo "<strong>" . $mensagem->nome . "</strong> disse: <em>" . $mensagem->mensagem . "</em><br />";
+                }
+            ?>
         </div>
     </div>
 </body>
